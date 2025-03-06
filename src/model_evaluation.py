db1601794a012b5360d18d2133854e6f0e9a03b1
Aditya <mt1221953@maths.iitd.ac.in>
2025-03-06 22:41:23 +0530

    Pipeline ready

---
+++ src/model_evaluation.py
@@ -0,0 +1,92 @@
+import os 
+import numpy as np
+import pandas as pd
+import pickle
+import json
+from sklearn.metrics import accuracy_score,precision_score,roc_auc_score
+import logging
+
+
+log_dir='logs'
+os.makedirs(log_dir,exist_ok=True)
+
+logger=logging.getLogger('model_evaluation')
+logger.setLevel('DEBUG')
+
+console_handler=logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path=os.path.join(log_dir,'model_evaluation.log')
+file_handler=logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formater=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formater)
+file_handler.setFormatter(formater)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_model(file_path: str):
+    try:
+        with open(file_path, 'rb') as file:
+            model=pickle.load(file)
+        logger.debug("model loaded")
+        return model
+    except FileNotFoundError:
+        logger.error('File not found')
+        raise
+    except Exception as e:
+        logger.error("unexpected error occured while loading the model")
+        raise
+
+def load_data(file_path :str)->pd.DataFrame:
+    try:
+        df=pd.read_csv(file_path)
+        logger.debug('Data loaded from')
+        return df
+    except pd.errors.ParserError as e:
+        logger.error("failed to parse the csv file")
+        raise
+    except Exception as e:
+        logger.error('unexpected error occured')
+        raise
+
+
+def evaluate_model(clf,x_test :np.ndarray,y_test:np.ndarray)->dict:
+    y_pred=clf.predict(x_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    metrics_dict={'accuracy':accuracy}
+    logger.debug("model evaluation metrics calculated")
+    return metrics_dict
+
+
+def save_metrics(metrics :dict,file_path:str)->None:
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'w') as file:
+            json.dump(metrics,file,indent=4)
+        logger.debug('Metrics saved to %s',file_path)
+    except Exception as e:
+        logger.error('Error occured while saving %s',e)
+        raise
+
+def main():
+    try :
+        clf=load_model('./models/model.pkl')
+        test_data=load_data('./data/processed/test_tfidf.csv')
+
+        x_test=test_data.iloc[:,:-1].values
+        y_test=test_data.iloc[:,-1].values
+
+        metrics=evaluate_model(clf,x_test,y_test)
+
+        save_metrics(metrics,'reports/metrics.json')
+    except Exception as e:
+        logger.error("failed")
+        raise
+
+if __name__=='__main__':
+    main()
+
+    
---
+++ src/model_train.py
@@ -0,0 +1,82 @@
+import os 
+import numpy as np
+import pandas as pd
+import pickle
+import logging
+from sklearn.ensemble import RandomForestClassifier
+
+log_dir='logs'
+os.makedirs(log_dir,exist_ok=True)
+
+logger=logging.getLogger('model_train')
+logger.setLevel('DEBUG')
+
+console_handler=logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path=os.path.join(log_dir,'model_train.log')
+file_handler=logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formater=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formater)
+file_handler.setFormatter(formater)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_data(file_path:str)->pd.DataFrame:
+    try:
+        df=pd.read_csv(file_path)
+        # df.fillna('',inplace=True)
+        logger.debug('Data loaded and Nans filled from %s',file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file : %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occured while loading the data: %s',e)
+        raise 
+
+def train_model(x_train: np.ndarray,y_train: np.ndarray,params: dict) -> RandomForestClassifier:
+    try:
+        if x_train.shape[0] != y_train.shape[0]:
+            raise ValueError("Not same")
+        logger.debug("intialise")
+        clf=RandomForestClassifier(n_estimators=params['n_estimators'],random_state=params['random_state'])
+
+        clf.fit(x_train,y_train)
+        return clf
+    except ValueError as e:
+        logger.error("Value - error during model training: %s",e)
+        raise
+
+def save_model(model,file_path: str) ->None:
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'wb') as file:
+            pickle.dump(model,file)
+        logger.debug('Model saved to : %s',file_path)
+    except FileNotFoundError as e:
+        logger.error('File path not found : %s',e)
+        raise
+    except Exception as e:
+        logger.error("occured error")
+        raise
+def main():
+    try:
+        params={'n_estimators':25,'random_state':2}
+        train_data=load_data('./data/processed/train_tfidf.csv')
+        x_train=train_data.iloc[:,:-1].values
+        y_train=train_data.iloc[:,-1].values
+        clf =train_model(x_train,y_train,params)
+        model_save_path='models/model.pkl'
+        save_model(clf,model_save_path)
+
+    except Exception as e:
+        logger.error('Faield')
+
+if __name__=='__main__':
+    main()
+
